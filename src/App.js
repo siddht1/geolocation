22de8c4c109318610996d5e40ac7f64255fe9b97
siddht1 <rt1yrru+github@gmail.com>
2024-06-09 20:36:51 +0530

    add location

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GetLocation from './GetLocation';
+
+function App() {
+  return (
+    <div className="App">
+      <GetLocation />
+    </div>
+  );
+}
+
+export default App;
